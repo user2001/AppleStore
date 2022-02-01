df7be89159baa533874fecfcac34c4514a6f47c7
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-02-01 20:55:38 +0200

    тепер можна обирати продукти і вони записуютьс вліст

--- src/com/company/DB/DBConnection.java
+++ src/com/company/DB/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DBConnection {
-    public  static final String URL = "jdbc:h2:D:\\IDEAProjects\\AppleStore\\db";
+    public  static final String URL = "jdbc:h2:C:/Users/Ivanna/IdeaProjects/AppleStore/db";
     public  static final String LOGIN = "login";
     public static final String PASSWORD = "password";
 
--- src/com/company/Guest.java
+++ src/com/company/Guest.java
@@ -4,43 +4,43 @@ import com.company.DB.AdminRepository;
 import com.company.DB.ProductRepository;
 import com.company.DB.UserRepository;
 import com.company.Entity.Admin;
+import com.company.Entity.Order;
 import com.company.Entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Guest implements LoginAsGuest {
-
     Scanner scanner = new Scanner(System.in);
 
 
     public void helloMenu() {
         System.out.println("Products menu-'1'");
         System.out.println("My orders menu-'2'");
-        System.out.println("Admins list-'3'");
+        System.out.println("Корзина-'3'");
         System.out.println("Exit-'0'");
     }
 
-
-    public void choiceRole(){
-        Guest user = new Guest();
+    public void choiceRole() {
         while (true) {
             helloMenu();
             int chooseNumber = scanner.nextInt();
             switch (chooseNumber) {
-                case 1 :
-                        chooseProducts();
+                case 1:
+                    showProducts();
                     break;
-                case 2 :
+                case 2:
+                    chooseProduct();
+                    break;
+                case 3:
 
+                    // id замовлень. меню адміна. переглянутию підтвердити. написати
                     break;
-                case 3 :
-                    showAdmins();
-                    break;
-                case 0 :
+                case 0:
                     System.exit(0);
-                default :
+                default:
                     System.out.println("Error, wrong answer");
                     helloMenu();
             }
@@ -66,18 +66,32 @@ public class Guest implements LoginAsGuest {
         }
     }
 
-    public void chooseProducts() {
+    public void showProducts() {
         ProductRepository productRepository = new ProductRepository();
         List<Product> productList;
         productList = productRepository.get();
         System.out.println(productList);
     }
 
-    public void confirmOrder() {
+    public void chooseProduct() {
+        ProductRepository productRepository = new ProductRepository();
+        List<Product> productList;
+        productList = productRepository.get();
+        System.out.println("Вкажіть id товару");
+        int needProduct = scanner.nextInt();
+        var choosenProducts = productList.stream()
+                .filter(product -> product.getId() == needProduct)
+                .collect(Collectors.toList());
+        System.out.println("Ви додали до вашої корзини: " + choosenProducts);
+    }
+
+
+    public void myCart() {
+
 
     }
 
-    public void showAdmins(){
+    public void showAdmins() {
         AdminRepository adminRepository = new AdminRepository();
         List<Admin> adminList;
         adminList = adminRepository.get();
