3efbf3b9f643103382dca91e3f3a385cf71c5e9d
Admin <kolia.osadchukcool@gmail.com>
2022-02-03 16:02:27 +0200

    some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import com.company.Menu.LoginMenu;
 
 public class Main {
 
-
+    //при запуску поміняйти шлях в DBConnection а свій щоб не було помилок
     public static void main(String[] args) {
         LoginMenu loginMenu = new LoginMenu();
         loginMenu.choiceRole();
