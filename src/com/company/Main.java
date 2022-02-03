1ccf4f0def0f2e149c950b09534e5658f77979bf
Admin <kolia.osadchukcool@gmail.com>
2022-02-03 15:58:47 +0200

    some changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,8 @@ public class Main {
     public static void main(String[] args) {
         LoginMenu loginMenu = new LoginMenu();
         loginMenu.choiceRole();
-
-
-//        orderRepository.makeOrder(new Order(2, list,12000));
-//        System.out.println(orderList);
     }
 
 }
-//    public static void main(String[] args) throws Exception {
-//        LoginMenu loginMenu = new LoginMenu();
-//        loginMenu.choiceRole();
-//    }
+
 
